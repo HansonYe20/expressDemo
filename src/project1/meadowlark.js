07c697f90d276b4681f59b518a81709b4fdebc00
yj15469 <yj15469@ly.com>
2019-10-21 18:08:57 +0800

    cookie, form, file upload

--- src/project1/meadowlark.js
+++ src/project1/meadowlark.js
@@ -3,7 +3,8 @@ const app = express();
 const path = require('path');
 const fortune = require('./lib/fortune.js');
 const weatherData = require('./lib/weatherData.js');
-
+const formidable = require('formidable');
+const credentials = require('./lib/credentials.js');
 
 app.set('port', process.env.PORT || 3000);
 
@@ -15,17 +16,28 @@ app.use(function (req, res, next) {
   next();
 });
 
+app.use(require('body-parser')());
+
+app.use(require('cookie-parser')(credentials.cookieSecret));
+
 app.use(express.static(__dirname + '/public')); // static中间件, 1.放在所有路由之前 2. public在链接上直接访问, 无需加/public这一层
 
 // 设置 handlebars 视图引擎
 const exphbs = require('express3-handlebars').create({
   defaultLayout: 'main',
   layoutsDir: path.join(__dirname, 'views/layouts'), // 由于本地的代码修改位置了, 所以要手动指定模板路劲
-  partialsDir: // path.join(__dirname, 'views/partials'),
-   [
-    //  path to your partials
-    path.join(__dirname, 'views/partials'),
-  ],
+  partialsDir: path.join(__dirname, 'views/partials'),
+  helpers: {
+    section: function (name, options) {
+      console.log(name);
+      console.log(JSON.stringify(options));
+      if (!this._sections) {
+        this._sections = {}
+      };
+      this._sections[name] = options.fn(this);
+      return null;
+    }
+  }
 });
 
 app.engine('handlebars', exphbs.engine);
@@ -33,6 +45,9 @@ app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(function (req, res, next) {
+  res.cookie('monster', Math.random());
+  res.cookie('signed_monster', 'nom nom', { signed: true });
+
   if (!res.locals.partials) { // locals 上下文环境
     res.locals.partials = {};
   }
@@ -43,6 +58,50 @@ app.use(function (req, res, next) {
 
 app.get('/', function (req, res) {
   res.render('home');
+});
+
+app.get('/contest/vacation-photo', function (req, res) {
+  var now = new Date();
+  res.render('contest/vacation-photo', {
+    year: now.getFullYear(),
+    month: now.getMonth()
+  });
+});
+app.post('/contest/vacation-photo/:year/:month', function (req, res) {
+  var monster = req.cookies.monster;
+  var signedMonster = req.signedCookies.monster;
+
+  var form = new formidable.IncomingForm();
+  form.parse(req, function (err, fields, files) {
+    if (err) return res.redirect(303, '/error');
+    console.log('received fields:');
+    console.log(fields);
+    console.log('received files:');
+    console.log(files);
+    res.redirect(303, '/thanks');
+  });
+});
+
+app.get('/newsletter', function (req, res) {
+  // 我们会在后面学到 CSRF......目前，只提供一个虚拟值
+  res.render('newsletter', { csrf: 'CSRF token goes here' });
+});
+
+// app.post('/process', function (req, res) {
+//   console.log('Form (from querystring): ' + req.query.form);
+//   console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+//   console.log('Name (from visible form field): ' + req.body.name);
+//   console.log('Email (from visible form field): ' + req.body.email);
+//   res.redirect(303, '/thanks');
+// });
+app.post('/process', function (req, res) {
+  if (req.xhr || req.accepts('json,html') === 'json') {
+    // 如果发生错误，应该发送 { error: 'error description' }
+    res.send({ success: true });
+  } else {
+    // 如果发生错误，应该重定向到错误页面
+    res.redirect(303, '/thanks');
+  }
 });
 
 app.get('/tours/hood-river', function (req, res) {
@@ -57,6 +116,10 @@ app.get('/about', function (req, res) {
     fortune: fortune.getFortune(), // 传递参数
     pageTestScript: '/qa/tests-about.js'
   });
+});
+
+app.get('/thanks', function (req, res) {
+  res.render('thanks');
 });
 
 // 404 catch-all 处理器(中间件) 
